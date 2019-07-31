ed8cdd145cc428c1ba63beb97a05b92223ce2f0a
John Chen <chenjiapeng12345@gmail.com>
2019-07-31 17:51:09 -0400

    add validations for clock_event

--- app/models/clock_event.rb
+++ app/models/clock_event.rb
@@ -1,3 +1,13 @@
 class ClockEvent < ApplicationRecord
+  validates :checkin_at, :user_id, presence: true
+  validates :checkout_at_cannot_before_checkin_at
+
   belongs_to :user
+
+  private
+  def checkout_at_cannot_before_checkin_at
+    if checkout_at.present? && checkout_at <= checkin_at
+      errors.add(:checkout_at, "cannot be before checkin time")
+    end
+  end
 end
