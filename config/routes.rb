f0a4221f67dff68ce88326d808b1e19befe055dc
John Chen <chenjiapeng12345@gmail.com>
2019-07-30 23:45:10 -0400

    update routes for sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
 end
