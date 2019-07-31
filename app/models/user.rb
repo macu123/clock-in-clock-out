a1c7cb13575f29adf4df198b616d21a315617304
John Chen <chenjiapeng12345@gmail.com>
2019-07-31 00:20:25 -0400

    add validations for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,12 @@
 class User < ApplicationRecord
   has_secure_password
 
+  validates :first_name, :last_name, :email, presence: true
+  validates :first_name, uniqueness: {
+    scope: :last_name,
+    message: "should be unique with same last name"
+  }
+  validates :email, uniqueness: true
+
   has_many :clock_events
 end
