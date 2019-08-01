e9b4a3d1857ccde57889aaa772033c34df5737fc
John Chen <chenjiapeng12345@gmail.com>
2019-08-01 01:43:46 -0400

    use url instead

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = user.id
-      redirect_to '/', notice: "You have signed up successfully."
+      redirect_to clock_events_url, notice: "You have signed up successfully."
     else
       render :new
     end
