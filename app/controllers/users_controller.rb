e5045196068f087059631e04bb60d4d61d1fabff
John Chen <chenjiapeng12345@gmail.com>
2019-08-01 09:40:17 -0400

    fix typo in users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      session[:user_id] = user.id
+      session[:user_id] = @user.id
       redirect_to clock_events_url, notice: "You have signed up successfully."
     else
       render :new
